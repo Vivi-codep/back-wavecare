a43ac36a6212bba74f8f89f2ec8d48d730698eac
Isabelle Santana <isabellexdev@gmail.com>
2026-04-30 18:48:57 -0300

    atualizando caminhos para conexão com o front

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
